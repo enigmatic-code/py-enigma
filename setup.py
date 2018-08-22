5370bde607c8ad25940884371a5373fe465131b5
Jim Randell <jim.randell@gmail.com>
2018-08-22 09:53:27 +0100

    setup.py to allow installation directly from GitHub.

---
+++ setup.py
@@ -0,0 +1,9 @@
+# this file allows enigma.py to be installed directly from GitHub
+
+from distutils.core import setup
+
+setup(
+  name='enigma',
+  version='2018-08-05',
+  py_modules=['enigma'],
+)
