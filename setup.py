6865ddb6d4ac7eb60334c4bf856fed04d0241de8
Jim Randell <jim.randell@gmail.com>
2018-08-22 10:21:51 +0100

    working version.

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 # this file allows enigma.py to be installed directly from GitHub
+#
+# using the following requirements.txt file:
+#
+# --
+# git+https://github.com/enigmatic-code/py-enigma.git#egg=enigma
+# --
 
 from distutils.core import setup
 
