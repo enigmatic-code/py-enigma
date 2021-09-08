a618b5d0fe6750740058cead0a0ce8e7f731379b
Jim Randell <jim.randell@gmail.com>
2021-09-08 16:56:14 +0100

    bump version number.

--- enigma.py
+++ enigma.py
@@ -6,7 +6,7 @@
 # Description:  Useful routines for solving Enigma Puzzles
 # Author:       Jim Randell
 # Created:      Mon Jul 27 14:15:02 2009
-# Modified:     Wed Sep  8 13:04:13 2021 (Jim Randell) jim.randell@gmail.com
+# Modified:     Wed Sep  8 16:56:05 2021 (Jim Randell) jim.randell@gmail.com
 # Language:     Python
 # Package:      N/A
 # Status:       Free for non-commercial use
@@ -165,7 +165,7 @@ Timer                  - a class for measuring elapsed timings
 from __future__ import print_function, division
 
 __author__ = "Jim Randell <jim.randell@gmail.com>"
-__version__ = "2021-09-06"
+__version__ = "2021-09-07"
 
 __credits__ = """Brian Gladman, contributor"""
 
