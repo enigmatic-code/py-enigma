d2d87661a94f67d432835e1629d9ff34c0353fc5
Jim Randell <jim.randell@gmail.com>
2021-09-17 09:28:45 +0100

    doc tweak.

--- enigma.py
+++ enigma.py
@@ -6,7 +6,7 @@
 # Description:  Useful routines for solving Enigma Puzzles
 # Author:       Jim Randell
 # Created:      Mon Jul 27 14:15:02 2009
-# Modified:     Thu Sep 16 13:51:42 2021 (Jim Randell) jim.randell@gmail.com
+# Modified:     Fri Sep 17 09:25:14 2021 (Jim Randell) jim.randell@gmail.com
 # Language:     Python
 # Package:      N/A
 # Status:       Free for non-commercial use
@@ -167,7 +167,7 @@ Timer                  - a class for measuring elapsed timings
 from __future__ import print_function, division
 
 __author__ = "Jim Randell <jim.randell@gmail.com>"
-__version__ = "2021-09-15"
+__version__ = "2021-09-16"
 
 __credits__ = """Brian Gladman, contributor"""
 
@@ -10181,7 +10181,7 @@ enigma.py has the following command-line usage:
     (KBKGEQD + GAGEEYQ + ADKGEDY = EXYAAEE)
     (1912803 + 2428850 + 4312835 = 8654488) / A=4 B=9 D=3 E=8 G=2 K=1 Q=0 X=6 Y=5
 
-""".format(version=__version__, python='2.7.18', python3='3.9.6')
+""".format(version=__version__, python='2.7.18', python3='3.9.7')
 
 def _namecheck(name, verbose=0):
   if verbose or ('v' in _PY_ENIGMA): printf("[_namecheck] checking \"{name}\"")
