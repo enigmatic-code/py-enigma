b6ae1e851f522b51b79e20a13e7633a2c7e03c1b
Jim Randell <jim.randell@gmail.com>
2022-03-25 21:43:04 +0000

    verified against Python 3.10.4.

--- enigma.py
+++ enigma.py
@@ -6,7 +6,7 @@
 # Description:  Useful routines for solving Enigma Puzzles
 # Author:       Jim Randell
 # Created:      Mon Jul 27 14:15:02 2009
-# Modified:     Wed Mar 23 17:33:56 2022 (Jim Randell) jim.randell@gmail.com
+# Modified:     Fri Mar 25 18:22:48 2022 (Jim Randell) jim.randell@gmail.com
 # Language:     Python
 # Package:      N/A
 # Status:       Free for non-commercial use
@@ -206,7 +206,7 @@ Timer                  - a class for measuring elapsed timings
 from __future__ import print_function, division
 
 __author__ = "Jim Randell <jim.randell@gmail.com>"
-__version__ = "2022-03-23"
+__version__ = "2022-03-25"
 
 __credits__ = """Brian Gladman, contributor"""
 
@@ -550,7 +550,7 @@ def zip_eq(*ss, **kw):
 
   >>> zip_eq((1, 2, 3), [1, 2, 3])
   True
-  >>> zip_eq((1, 2, 3), [1, 2, 3], map(int, "123"))
+  >>> zip_eq((1, 2, 3), [1, 2, 3], irange(1, 3), map(int, "123"))
   True
   >>> zip_eq((1, 2, 3, 4), [1, 2, 4, 8])
   False
@@ -10799,7 +10799,7 @@ enigma.py has the following command-line usage:
     (KBKGEQD + GAGEEYQ + ADKGEDY = EXYAAEE)
     (1912803 + 2428850 + 4312835 = 8654488) / A=4 B=9 D=3 E=8 G=2 K=1 Q=0 X=6 Y=5
 
-""".format(version=__version__, python='2.7.18', python3='3.10.3')
+""".format(version=__version__, python='2.7.18', python3='3.10.4')
 
 def _namecheck(name, verbose=0):
   if verbose or ('v' in _PY_ENIGMA): printf("[_namecheck] checking \"{name}\"")
