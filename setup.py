757ee8a834b7a43c5a98647d9f67f383279e7ee3
Jim Randell <jim.randell@gmail.com>
2023-07-06 15:43:20 +0100

    autogenerated

--- setup.py
+++ setup.py
@@ -2,39 +2,61 @@
 #
 #
 # [install using pip and requirements.txt]
+#
 # add the following line to a requirements.txt file:
+#
 # --
-# enigma @ git+https://github.com/enigmatic-code/py-enigma
+# enigma@git+https://github.com/enigmatic-code/py-enigma
 # --
+#
+# or:
+#
+# --
+# enigma@https://github.com/enigmatic-code/py-enigma/tarball/master
+# --
+#
 # and then run the following command to install/upgrade:
+#
 # --
-# % pip3 install -U -r requirements.txt
+# % python3 -m pip install -U -r requirements.txt
 # --
 #
 #
 # [install/upgrade directly using pip (with git)]
+#
 # --
-# % pip3 install -U "enigma @ git+https://github.com/enigmatic-code/py-enigma"
+# % python3 -m pip install -U enigma@git+https://github.com/enigmatic-code/py-enigma
 # --
 #
 #
 # [install/upgrade directly using pip (without git)]
+#
 # --
-# % pip3 install -U "enigma @ https://github.com/enigmatic-code/py-enigma/tarball/master"
+# % python3 -m pip install -U enigma@https://github.com/enigmatic-code/py-enigma/tarball/master
 # --
 #
 #
 # [using Poetry]
+#
 # add the following in pyproject.toml file:
+#
 # --
 # [tool.poetry.dependencies.enigma]
 # git = "https://github.com/enigmatic-code/py-enigma.git"
+# --
+#
+# or:
+#
+# --
+# [tool.poetry.dependencies.enigma]
+# url = "https://github.com/enigmatic-code/py-enigma/tarball/master"
+# --
 
 from setuptools import setup
 
 setup(
   name='enigma',
-  version='2.7.20230628',
+  version='2.7.20230704',
   description='Useful routines for solving New Scientist Enigma (and other) puzzles',
   author='Jim Randell',
   author_email='jim.randell@gmail.com',
