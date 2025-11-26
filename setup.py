aa86ae868bd68b8fcc637f973305e3ef5704f255
Jim Randell <jim.randell@gmail.com>
2025-11-26 08:17:34 +0000

    autogenerated

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ from setuptools import setup
 
 setup(
   name='enigma',
-  version='2.7.20251124',
+  version='2.7.20251125',
   description='Useful routines for solving New Scientist Enigma (and other) puzzles',
   author='Jim Randell',
   author_email='jim.randell@gmail.com',
